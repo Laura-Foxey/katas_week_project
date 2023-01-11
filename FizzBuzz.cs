640d4c0d94ded6c655a7388a605c7b438cd3a4f9
Laura.Foxey <roura_sasunaru@hotmail.com>
2023-01-11 23:34:58 +0000

    create fizzbuzz project

---
+++ FizzBuzz.cs
@@ -0,0 +1,9 @@
+/*
+Create a function called "FizzBuzz" that takes in an integer n as a parameter and prints the numbers 
+    from 1 to n, replacing numbers divisible by 3 with "Fizz", numbers divisible by 5 with "Buzz", 
+    and numbers divisible by both 3 and 5 with "FizzBuzz". 
+*/
+
+public class Solution {
+
+}
